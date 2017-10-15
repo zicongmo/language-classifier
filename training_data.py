710074847280801be785f66a3738cc9bd630fdc7
Joseph Mo <josephmo@Josephs-MacBook-Pro.local>
2017-10-15 14:39:26 -0700

    Changed convert_word to return one-hot version of input string

--- training_data.py
+++ training_data.py
@@ -16,16 +16,32 @@ class TrainingLanguageData:
                 self.data[word[:-1]] = 0
         # The order to use when generating the next batch of training examples
         self.items = list(self.data.items())
-        self.index = 0
-        random.shuffle(self.items)
-
+        self.randomize_dict()
+        
         # Number of training examples provided
         self.num_examples = len(self.items)
 
     # Converts string to one-hot
     def convert_word(self, string):
-        return string
+        arr = []
+        for i in range(len(string)):
+            lst = []
+            char_code = ord(string[i])
+            # If the character is between a-z, no other characters exist :)
+            if (char_code >= 97) and (char_code <= 122):
+                for letter in range(97, 123):
+                    if letter == char_code:
+                        lst.append(1)
+                    else:
+                        lst.append(0)
+                arr.extend(lst)
+        return arr
 
+    # Resets index and randomizes the order of the items to iterate through
+    def randomize_dict(self):
+        self.index = 0
+        random.shuffle(self.items)
+    
     # Return the next n training examples as a list of lists
     def get_next_batch(self, n):
         batch = []
@@ -33,12 +49,18 @@ class TrainingLanguageData:
         # Not enough training examples left to complete request, give remaining
         if upper_limit > self.num_examples:
             upper_limit = self.num_examples
-        for i in range(self.index, upper_limit):
-            word = self.items[i][0]
-            value = self.items[i][1]
+            
+        while self.index < upper_limit:
+            pair = self.items[self.index]
+            word = pair[0]
+            value = pair[1]
             lst = [self.convert_word(word), value]
             batch.append(lst)
             self.index += 1
+            
+        if self.index == self.num_examples:
+            self.randomize_dict()
+            
         return batch
         
 data = TrainingLanguageData("English.txt", "Spanish.txt")
