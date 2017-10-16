3185057b22bacfa1d8a64a2d1fe91fcc970f0ce9
Joseph Mo <josephmo@Josephs-MacBook-Pro.local>
2017-10-15 21:17:59 -0700

    Fixed a lot of things to make it actually work. Changed activation function to improve accuracy

--- language_identifier.py
+++ language_identifier.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from training_data import LanguageTrainingData
+
+data = LanguageTrainingData("English.txt", "Spanish.txt")
 
 # classify between English and Spanish
 num_languages = 2
@@ -11,11 +14,13 @@ num_nodes_l2 = 500
 batch_size = 100
 
 # Number of training epochs
-num_epochs = 20
+num_epochs = 1000
 
 # Dimension of input vector
-num_features = 100
+num_features = 260
 
+x = tf.placeholder("float", shape=[None, num_features])
+y = tf.placeholder("float", shape=[None, num_languages])
 
 # input_data is a num_features dimensional vector
 def neural_network(input_data):
@@ -28,31 +33,41 @@ def neural_network(input_data):
                      "biases": tf.Variable(tf.random_normal([num_languages]))}
 
     # Feed the input of each layer through the layer until the output layer
-    output_l1 = tf.matmul(input_data, layer1["weights"]) + layer1["biases"]
-    output_l1 = tf.nn.tanh(output_l1)
+    output_l1 = tf.add(tf.matmul(input_data, layer1["weights"]) , layer1["biases"])
+    output_l1 = tf.nn.relu(output_l1)
                                                                   
-    output_l2 = tf.matmul(output_l1, layer2["weights"]) + layer2["biases"]
-    output_l2 = tf.nn.tanh(output_l2)
+    output_l2 = tf.add(tf.matmul(output_l1, layer2["weights"]) , layer2["biases"])
+    output_l2 = tf.nn.relu(output_l2)
 
-    output = tf.matmul(output_l2, output_layer["weights"]) + output_layer["biases"]
-    output = tf.nn.tanh(output)
-
+    output = tf.add(tf.matmul(output_l2, output_layer["weights"]) , output_layer["biases"])
+    
     return output
 
 # input_data is a list containing all of the training data
-def train(input_data):
-    output = neural_network(input_data)
+def train(x):
+    prediction = neural_network(x)
     # Computes the cross entropy between prediction and y, and takes the average
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
 
     # Change the NN variables with the Adam algorithm to minimize the value of cost
     optimizer = tf.train.AdamOptimizer().minimize(cost)
 
-                          
-                          
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
-    
+        for epoch in range(num_epochs):
+            total_loss = 0
+            for i in range(int(data.num_examples/batch_size)):
+                batch_x, batch_y = data.get_next_batch(batch_size)
+                i, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+                total_loss += c
+            if epoch % 100 == 0:
+                print("Epoch", epoch, "completed with loss:", total_loss)
 
-
-
+        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+        batch_x, batch_y = data.get_test_data()
+        print('Accuracy:', accuracy.eval({x: batch_x, y: batch_y}))
+            
+train(x)
         
--- training_data.py
+++ training_data.py
@@ -1,37 +1,46 @@
 import random
+import numpy as np
 
 class LanguageTrainingData:
     # The names of the files containing language data for 2 languages
     def __init__(self, language_1, language_2):
         self.data = {}
         self.test_data = {}
+        self.words_in_language_1 = 0
+        self.words_in_language_2 = 0
+        self.test_words_in_l1 = 0
         # If a word is in both files, the word is assigned language_2
         with open(language_1) as file:
             words = file.readlines()
             i = 0
             for word in words:
                 # ignore the newline character
-                if i % 10 == 0:
-                    self.test_data[word[:-1]] = 1
-                else:
-                    self.data[word[:-1]] = 1
-                i += 1
+                if len(word) <= 10:
+                    if i % 10 == 0:
+                        self.test_data[word[:-1]] = 1
+                        self.test_words_in_l1 += 1
+                    else:
+                        self.data[word[:-1]] = 1
+                    self.words_in_language_1 += 1
+                    i += 1
         with open(language_2) as file:
             words = file.readlines()
             i = 0
             for word in words:
-                # ignore the newline character
-                if i % 10 == 0:
-                    self.test_data[word[:-1]] = 0
-                else:
-                    self.data[word[:-1]] = 0
-                i += 1
+                if len(word) <= 10:
+                    if i % 10 == 0:
+                        self.test_data[word[:-1]] = 0
+                    else:
+                        self.data[word[:-1]] = 0
+                    self.words_in_language_2 += 1
+                    i += 1
         # The order to use when generating the next batch of training examples
         self.items = list(self.data.items())
         self.randomize_dict()
         
         # Number of training examples provided
         self.num_examples = len(self.items)
+        self.num_tests = len(self.test_data)
 
     # Converts string to one-hot
     def convert_word(self, string):
@@ -47,41 +56,57 @@ class LanguageTrainingData:
                     else:
                         lst.append(0)
                 arr.extend(lst)
+        # Fill the rest in with 0's
+        for i in range(len(arr), 260):
+            arr.append(0)
+
         return arr
+
+    # Converts label to one-hot
+    def convert_label(self, label):
+        if label == 1:
+            return [0, 1]
+        return [1, 0]
 
     # Resets index and randomizes the order of the items to iterate through
     def randomize_dict(self):
         self.index = 0
         random.shuffle(self.items)
     
-    # Return the next n training examples as a list of lists
+    # Return the next n training examples
     def get_next_batch(self, n):
-        batch = []
+        batch_x = np.zeros(shape=(100,260))
+        batch_y = np.zeros(shape=(100,2))
         upper_limit = self.index+n
         # Not enough training examples left to complete request, give remaining
         if upper_limit > self.num_examples:
             upper_limit = self.num_examples
-            
+
+        i = 0
         while self.index < upper_limit:
             pair = self.items[self.index]
             word = pair[0]
             value = pair[1]
-            lst = [self.convert_word(word), value]
-            batch.append(lst)
+            batch_x[i] = self.convert_word(word)
+            batch_y[i] = self.convert_label(value)
             self.index += 1
+            i += 1
             
         if self.index == self.num_examples:
             self.randomize_dict()
-            
-        return batch
+        
+        return batch_x, batch_y
 
     # Return all of the test data
     def get_test_data(self):
-        batch = []
+        batch_x = np.zeros(shape=(self.num_tests, 260))
+        batch_y = np.zeros(shape=(self.num_tests, 2))
+        i = 0
         for pair in self.test_data.items():
             word = pair[0]
             value = pair[1]
-            lst = [self.convert_word(word), value]
-            batch.append(lst)
-        return batch
+            batch_x[i] = self.convert_word(word)
+            batch_y[i] = self.convert_label(value)
+            i += 1
+        return batch_x, batch_y
 
